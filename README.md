99a0171e2033faab31638c65c03f123037f806ff
Mitchell Krog <mitchellkrog@gmail.com>
2018-04-20 14:22:42 +0200

    Add Readme Update Function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Phishing.Database
 Testing repository for Phishing sites and threats.
+
+_______________
+#### Version: 
+#### Total Phishing Domains: 
+____________________
+
--- dev-tools/generate.sh
+++ dev-tools/generate.sh
@@ -3,7 +3,6 @@
 # REPO: https://github.com/mitchellkrogza/Fail2Ban.WebExploits
 # Copyright Mitchell Krog - mitchellkrog@gmail.com
 
-tmplt=tmplt
 tmprdme=tmprdme
 tmprdme2=tmprdme2
 input=${TRAVIS_BUILD_DIR}/input-source/exploits.list
@@ -48,49 +47,9 @@ cut -d'/' -f3 ${output} > ${outputtmp}
 sort -u ${outputtmp} -o ${outputtmp}
 grep '[^[:blank:]]' < ${outputtmp} > ${output}
 sudo rm ${outputtmp}
-}
-
-# *******************************
-# Build the beginning of our file
-# *******************************
-
-generate () {
-printf '%s\n' "# Fail2Ban Web Exploits Filter" >> ${tmplt}
-printf '%s\n' "# Author & Copyright: Mitchell Krog - mitchellkrog@gmail.com" >> ${tmplt}
-printf '%s\n' "# REPO: https://github.com/mitchellkrogza/Fail2Ban.WebExploits" >> ${tmplt}
-printf '%s%s\n' "# " "${version}" >> ${tmplt}
-printf '%s%s\n\n' "# Last Updated: " "${versiondate}" >> ${tmplt}
-printf '%s\n' "[Definition]" >> ${tmplt}
-printf '\n\n' >> ${tmplt}
-printf '%s\n' "failregex = ^<HOST> -.*GET.*(/.git/config)" >> ${tmplt}
-
-# **************************************************************
-# Now loop through our input file and write the rest of the file
-# **************************************************************
-
-while IFS= read -r LINE
-do
-printf '%s%s%s%s\n' "            " "^<HOST> -.*GET.*(" "${LINE}" ")" >> ${tmplt}
-done < ${input}
-
-# *****************************
-# Now write the end of our file
-# *****************************
-
-printf '\n%s\n' "ignoreregex ="  >> ${tmplt}
-
-# *************************************
-# Move the temp file to the output file
-# *************************************
-
-mv ${tmplt} ${output}
-
-# *****************************************************
-# Activate Dos2Unix to make sure file is in Unix format
-# *****************************************************
-
 dos2unix ${output}
 }
+
 
 # **************************************************
 # Write Version and Exploit Count into the README.md
@@ -98,17 +57,17 @@ dos2unix ${output}
 
 updatereadme () {
 
-printf '%s\n%s%s\n%s%s\n%s' "${startmarker}" "#### Version: " "${version}" "#### Total Exploits: " "${totalexploits}" "${endmarker}" >> ${tmprdme}
+printf '%s\n%s%s\n%s%s\n%s' "${startmarker}" "#### Version: " "${version}" "#### Total Phishing Domains: " "${totalexploits}" "${endmarker}" >> ${tmprdme}
 mv ${tmprdme} ${tmprdme2}
 ed -s ${tmprdme2}<<\IN
 1,/_______________/d
 /____________________/,$d
 ,d
-.r /home/travis/build/mitchellkrogza/Fail2Ban.WebExploits/README.md
+.r /home/travis/build/mitchellkrogza/Phishing.Database/README.md
 /_______________/x
 .t.
 .,/____________________/-d
-w /home/travis/build/mitchellkrogza/Fail2Ban.WebExploits/README.md
+w /home/travis/build/mitchellkrogza/Phishing.Database/README.md
 q
 IN
 rm ${tmprdme2}
@@ -159,8 +118,7 @@ sudo git push origin master
 fetch
 initiate
 prepare
-#generate
-#updatereadme
+updatereadme
 commit
 
 # **********************
