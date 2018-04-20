3c2e71570bec1e15234cb9122f7d9731b4144a53
Mitchell Krog <mitchellkrog@gmail.com>
2018-04-20 16:30:20 +0200

    Modify Commit Script

--- dev-tools/commit.sh
+++ dev-tools/commit.sh
@@ -3,18 +3,18 @@
 # REPO: https://github.com/mitchellkrogza/Phishing.Database
 # Copyright Mitchell Krog - mitchellkrog@gmail.com
 
-# *******************************
-# Make sure Travis owns all files
-# *******************************
-
-sudo chown -R travis:travis ${TRAVIS_BUILD_DIR}/
-
 # ***********************************
 # Copy tested files into root of repo
 # ***********************************
 
 cat ${TRAVIS_BUILD_DIR}/dev-tools/PyFunceble/output/domains/ACTIVE/list | grep -v "^$" | grep -v "^#" > ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt
 cat ${TRAVIS_BUILD_DIR}/dev-tools/PyFunceble/output/domains/INACTIVE/list | grep -v "^$" | grep -v "^#" > ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt
+
+# *******************************
+# Make sure Travis owns all files
+# *******************************
+
+sudo chown -R travis:travis ${TRAVIS_BUILD_DIR}/
 
 # *********************************************************
 # Pull Fresh Data for our Next Tests and Modify Readme File
