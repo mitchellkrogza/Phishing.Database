6c0466e7205e0b3faf169272c7f9d09b42aec43a
funilrys <contact@funilrys.com>
2018-07-25 17:38:47 +0200

    Fix issue regarding the branch linking

--- dev-tools/pulldata.sh
+++ dev-tools/pulldata.sh
@@ -35,7 +35,7 @@ PrepareTravis () {
     git config --global user.email "${GIT_EMAIL}"
     git config --global user.name "${GIT_NAME}"
     git config --global push.default simple
-    git checkout master
+    git checkout "${GIT_BRANCH}"
 }
 
 # **********************************************
