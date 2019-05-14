e73de9edcd6788a11f544db4e2667517f4f7561d
Alex Williams <hughaw64@gmail.com>
2019-05-14 20:17:42 +0100

    Removed grepping

--- dev-tools/whitelist.sh
+++ dev-tools/whitelist.sh
@@ -1,13 +1,7 @@
-uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testingTEMP.txt -m -p 60
-uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVETEMP.txt -m -p 60
-uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testingTEMP.txt -m -p 60
-uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVETEMP.txt -m -p 60
-uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testingTEMP.txt -m -p 60
-uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALIDTEMP.txt
-grep . ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testingTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt
-grep . ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVETEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt
-grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testingTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt
-grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVETEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt
-grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testingTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt
-grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INVALIDTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt
-rm ${TRAVIS_BUILD_DIR}/phishing-domains*TEMP.txt
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt -m -p 60
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt -m -p 60
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt -m -p 60
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt -m -p 60
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt -m -p 60
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt
+
