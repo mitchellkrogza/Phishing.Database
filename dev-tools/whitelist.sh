902cfa613f4085668b715bc50d7b0055e726fed5
Alex Williams <hughaw64@gmail.com>
2019-05-14 16:19:31 +0100

    Used grep to remove all blank lines

--- dev-tools/whitelist.sh
+++ dev-tools/whitelist.sh
@@ -4,9 +4,10 @@ uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt -o
 uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVETEMP.txt
 uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testingTEMP.txt
 uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALIDTEMP.txt
-mv ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testingTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt
-mv ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVETEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt
-mv ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testingTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt
-mv ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVETEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt
-mv ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testingTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt
-mv ${TRAVIS_BUILD_DIR}/phishing-domains-INVALIDTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt
+grep . ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testingTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt
+grep . ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVETEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt
+grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testingTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt
+grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVETEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt
+grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testingTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt
+grep . ${TRAVIS_BUILD_DIR}/phishing-domains-INVALIDTEMP.txt >${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt
+rm ${TRAVIS_BUILD_DIR}/phishing-domains*TEMP.txt
