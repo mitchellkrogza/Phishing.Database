b7c3b9284862ffa6c0d96e22f61cde0842a22369
Alex Williams <hughaw64@gmail.com>
2019-05-14 16:17:50 +0100

    second part of suggestion

--- dev-tools/whitelist.sh
+++ dev-tools/whitelist.sh
@@ -1,12 +1,12 @@
-uhb_whitelist -f phishing-domains-ACTIVE-in-testing.txt -o phishing-domains-ACTIVE-in-testingTEMP.txt
-uhb_whitelist -f phishing-domains-ACTIVE.txt -o phishing-domains-ACTIVETEMP.txt
-uhb_whitelist -f phishing-domains-INACTIVE-in-testing.txt -o phishing-domains-INACTIVE-in-testingTEMP.txt
-uhb_whitelist -f phishing-domains-INACTIVE.txt -o phishing-domains-INACTIVETEMP.txt
-uhb_whitelist -f phishing-domains-INVALID-in-testing.txt -o phishing-domains-INVALID-in-testingTEMP.txt
-uhb_whitelist -f phishing-domains-INVALID.txt -o phishing-domains-INVALIDTEMP.txt
-mv phishing-domains-ACTIVE-in-testingTEMP.txt phishing-domains-ACTIVE-in-testing.txt
-mv phishing-domains-ACTIVETEMP.txt phishing-domains-ACTIVE.txt
-mv phishing-domains-INACTIVE-in-testingTEMP.txt phishing-domains-INACTIVE-in-testing.txt
-mv phishing-domains-INACTIVETEMP.txt phishing-domains-INACTIVE.txt
-mv phishing-domains-INVALID-in-testingTEMP.txt phishing-domains-INVALID-in-testing.txt
-mv phishing-domains-INVALIDTEMP.txt phishing-domains-INVALID.txt
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testingTEMP.txt
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVETEMP.txt
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testingTEMP.txt
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVETEMP.txt
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testingTEMP.txt
+uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INVALIDTEMP.txt
+mv ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testingTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE-in-testing.txt
+mv ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVETEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt
+mv ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testingTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE-in-testing.txt
+mv ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVETEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt
+mv ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testingTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID-in-testing.txt
+mv ${TRAVIS_BUILD_DIR}/phishing-domains-INVALIDTEMP.txt ${TRAVIS_BUILD_DIR}/phishing-domains-INVALID.txt
