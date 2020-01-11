476f9b0974b49dc30dc99cacaec4bd1e5133f12b
Travis <mitchellkrog@gmail.com>
2020-01-11 02:12:33 +0200

    V.16414 (2020-01-11 02:12:33 SAST) [ci skip]

--- dev-tools/db_types/mysql.sql
+++ dev-tools/db_types/mysql.sql
@@ -36,7 +36,7 @@
 --
 --     MIT License
 --
---     Copyright (c) 2017, 2018, 2019 Nissar Chababy
+--     Copyright (c) 2017, 2018, 2019, 2020 Nissar Chababy
 --
 --     Permission is hereby granted, free of charge, to any person obtaining a copy
 --     of this software and associated documentation files (the "Software"), to deal
