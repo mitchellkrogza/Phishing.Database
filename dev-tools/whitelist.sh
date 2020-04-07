353c6ad85eeec4b089394981c69ef04af1c8930d
Mitchell Krog <mitchellkrog@gmail.com>
2020-04-07 09:54:22 +0200

    Add sorting of whitelist files

--- dev-tools/whitelist.sh
+++ dev-tools/whitelist.sh
@@ -2,6 +2,10 @@
 # Whitelist Script for Phishing.Database
 # Contributed by Alex Williams - https://github.com/mkb2091
 
+# Sort Lists
+sort -u ${TRAVIS_BUILD_DIR}/whitelist.me/whitelist.me -o ${TRAVIS_BUILD_DIR}/whitelist.me/whitelist.me
+sort -u ${TRAVIS_BUILD_DIR}/whitelist.anti/whitelist.anti -o ${TRAVIS_BUILD_DIR}/whitelist.anti/whitelist.anti
+
 # WhiteList Domains
 uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-ACTIVE.txt -w ${TRAVIS_BUILD_DIR}/whitelist.me/whitelist.me
 uhb_whitelist -f ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt -o ${TRAVIS_BUILD_DIR}/phishing-domains-INACTIVE.txt -w ${TRAVIS_BUILD_DIR}/whitelist.me/whitelist.me
