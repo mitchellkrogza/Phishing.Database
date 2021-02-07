85ebc425e3036a1879c0352cf271591e243e10d8
Mitchell Krog <mitchellkrog@gmail.com>
2021-02-08 00:01:11 +0100

    V.2021-02-08.00 Repository Reset [skip ci]

---
+++ deprecated/dev-tools/_deprecated/debug-commit.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Travis CI Force Commits for Phishing.Database Repo
+# Created by: Mitchell Krog (mitchellkrog@gmail.com)
+# Copyright: Mitchell Krog - https://github.com/mitchellkrogza
+# Repo Url: https://github.com/mitchellkrogza/Phishing.Database/
+
+# MIT License
+
+# ******************
+# Set Some Variables
+# ******************
+
+YEAR=$(date +"%Y")
+MONTH=$(date +"%m")
+
+# *******************************
+# Setup Travis-CI GIT
+# *******************************
+
+cd ${TRAVIS_BUILD_DIR}
+git remote rm origin
+git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global push.default simple
+git checkout master
+
+# *************************************
+# Add all the modified files and commit
+# *************************************
+
+git add -A
+git commit -am "DEBUG.${TRAVIS_BUILD_NUMBER} [ci skip]"
+git push origin master
+
+# **********************
+# Exit With Error Number
+# **********************
+
+exit ${?}
+
+# MIT License
+
+# Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
+# https://github.com/mitchellkrogza
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
