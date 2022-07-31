029cb17495127d9424c3706de14a5b1caa032b52
funilrys <contact@funilrys.com>
2022-07-31 11:46:56 +0200

    Introduction of the nextdns/metadata urlshorteners.
    This patch fixes #300.
    
    Indeed, before this patch, url shorteneres may have been falsely
    flagged as Phishing URL.
    This patch solves the issue by reusing the great url-shorteners
    list from @nextdns during our whitelisting process.
    
    https://raw.githubusercontent.com/nextdns/metadata/master/security/url-shorteners
    
    Thank you @PeterDaveHello for the recommendation.
    
    Contributors:
      * @PeterDaveHello

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -5,6 +5,8 @@
 set -e
 set -o pipefail
 
+whitelistFiles="./whitelist.me/whitelist.me https://raw.githubusercontent.com/nextdns/metadata/master/security/url-shorteners"
+
 # UNTAR INPUT FILES
 #cd ./input-source/
 #ls -la
@@ -17,16 +19,16 @@ sort -u ./whitelist.me/whitelist.me -o ./whitelist.me/whitelist.me
 sort -u ./whitelist.anti/whitelist.anti -o ./whitelist.anti/whitelist.anti
 
 # WhiteList Domains
-uhb_whitelist -f ./phishing-domains-ACTIVE.txt -o ./phishing-domains-ACTIVE.txt -w ./whitelist.me/whitelist.me
-uhb_whitelist -f ./phishing-domains-INACTIVE.txt -o ./phishing-domains-INACTIVE.txt -w ./whitelist.me/whitelist.me
-uhb_whitelist -f ./phishing-domains-INVALID.txt -o ./phishing-domains-INVALID.txt -w ./whitelist.me/whitelist.me
-uhb_whitelist -f ./input-source/ALL-feeds.lst -o ./ALL-phishing-domains.txt -w ./whitelist.me/whitelist.me
+uhb_whitelist -f ./phishing-domains-ACTIVE.txt -o ./phishing-domains-ACTIVE.txt -w ${whitelistFiles}
+uhb_whitelist -f ./phishing-domains-INACTIVE.txt -o ./phishing-domains-INACTIVE.txt -w ${whitelistFiles}
+uhb_whitelist -f ./phishing-domains-INVALID.txt -o ./phishing-domains-INVALID.txt -w ${whitelistFiles}
+uhb_whitelist -f ./input-source/ALL-feeds.lst -o ./ALL-phishing-domains.txt -w ${whitelistFiles}
 
 # WhiteList Links
-uhb_whitelist -f ./phishing-links-ACTIVE.txt -o ./phishing-links-ACTIVE.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ./whitelist.me/whitelist.me
-uhb_whitelist -f ./phishing-links-INACTIVE.txt -o ./phishing-links-INACTIVE.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ./whitelist.me/whitelist.me
-uhb_whitelist -f ./phishing-links-INVALID.txt -o ./phishing-links-INVALID.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ./whitelist.me/whitelist.me
-uhb_whitelist -f ./input-source/ALL-feeds-URLS.lst -o ./ALL-phishing-links.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ./whitelist.me/whitelist.me
+uhb_whitelist -f ./phishing-links-ACTIVE.txt -o ./phishing-links-ACTIVE.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ${whitelistFiles}
+uhb_whitelist -f ./phishing-links-INACTIVE.txt -o ./phishing-links-INACTIVE.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ${whitelistFiles}
+uhb_whitelist -f ./phishing-links-INVALID.txt -o ./phishing-links-INVALID.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ${whitelistFiles}
+uhb_whitelist -f ./input-source/ALL-feeds-URLS.lst -o ./ALL-phishing-links.txt --anti-whitelist ./whitelist.anti/whitelist.anti -w ${whitelistFiles}
 
 # FIX Remove Orphaned Domains - Lines not containing a . 
 cat ./phishing-domains-ACTIVE.txt | sed '/\./!d' > ./phishing-domains-ACTIVE.tmp && mv ./phishing-domains-ACTIVE.tmp ./phishing-domains-ACTIVE.txt
